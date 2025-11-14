08cd30f36853d70019fd4f30c5c2018d2be1c21a
Mate Marschalko <webondevices@gmail.com>
2025-11-14 10:47:43 +0000

    feat: auto-generated Prisma schema and migration

---
+++ prisma/migrations/20251114104153_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "responses" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "answers" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
